a5fb04f13d5555cbd1283fdfe60432c4d722dc06
Maria O <mariaolokun@gmail.com>
2023-11-05 14:22:51 -0600

    python-code

---
+++ error_handling.py
@@ -0,0 +1,13 @@
+
+"""try:
+    print(a)
+except Exception as e:
+    print(e)
+  
+print("hello")
+"""
+try:
+    num =int(input("please enter a number: "))
+    print(num)
+except:
+    print("invalid entry please try again")
